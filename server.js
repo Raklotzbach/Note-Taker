887f03a10c7b6ba3bc9d21a5aa2fe0d6e9c2ac59
Ryan Klotzbach <ryanklotzbach@Ryans-iMac.local>
2022-07-20 20:05:38 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
