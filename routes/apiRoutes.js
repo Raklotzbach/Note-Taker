7dfc6eb1336209afc9589b759ae67fbf2a0e92fc
Ryan Klotzbach <ryanklotzbach@Ryans-iMac.local>
2022-07-25 09:31:41 -0700

    updates before submit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,44 @@
 const fs = require('fs');
+const app = require('express').Router();
+const uuid = require('../helpers/uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { response } = require('express');
+
+
+// GET route for retreiving notes
+app.get('/notes', (req, res) => {
+    console.info(`${req.method} Request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
+
+
+// POST route for posting new note
+app.post('/notes', (req, res) => {
+    console.info(`${req.method} Request received to add new note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            noteId: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: "Success",
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json("Error posting Note");
+    }
+});
+
+
+
+module.exports=app;
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes')
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/api', apiRoutes);
 
 app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 app.listen(PORT, () =>
